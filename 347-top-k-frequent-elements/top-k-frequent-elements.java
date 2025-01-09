a52264ad9d791cc56113a3866242048e4e54ce60
MAHENDRA RAJAN <147966089+Mahendran8080@users.noreply.github.com>
2025-01-09 23:31:23 +0530

    Time: 17 ms (13.78%) | Memory: 48.7 MB (47.08%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -0,0 +1,52 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+
+        Map<Integer,Integer> map=new HashMap<>();
+
+        for(int num:nums)
+        {
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+
+        int[] arr=new int[map.size()];
+        int idx=0;
+
+        for(Map.Entry<Integer,Integer> entry:map.entrySet())
+        {
+            arr[idx++]=entry.getValue();
+        }
+
+        Arrays.sort(arr);
+
+    List<Integer> list=new ArrayList<>();
+    int i=arr.length-1;
+
+    while(k>0)
+    {
+        list.add(arr[i]);
+        k--;
+        i--;
+
+    }
+
+    int[] res=new int[list.size()];
+
+    int idx1=0;
+    int y=0;
+
+      for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (list.contains(entry.getValue())) { // Check if the frequency is in the top-k list
+                res[idx1++] = entry.getKey();
+                list.remove(entry.getValue()); // Remove the matched frequency to avoid duplicates
+                if (list.isEmpty()) {
+                    break; // Exit early if all top-k frequencies are matched
+                }
+            }
+        }
+
+     return res;
+
+
+        
+    }
+}
